cdff0b1ac6798043decf52d367e7661017cacc6f
Eliezer134 <eliezer134@gmail.com>
2022-09-03 00:48:17 -0500

    Ultimo commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 // El componente App es el padre de:
+import { useState } from 'react';
 // - Cabecera
+import Cabecera from "./components/Cabecera"
 // - Listado
+import Listado from "./components/Listado"
 // ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
+
+
+
+
 function App() {
+
+    const [compras, setCompras] = useState(0);
+
+    const suma=()=>{
+      setCompras(compras+1);
+    }
 
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera compras={compras}/>
+      <Listado suma={suma}/>
     </div>
   );
 }
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from "react";
+
+import Item from "./Item";
 
 // Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
+import dataJSON from "./data.json";
 // El componente Listado es el padre de:
 // - varios Item
 // ESTADO: Listado no necesita manejar un estado.
 // MÉTODOS: Listado no requiere de métodos.
 // PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
+/*/map((elemento) => {
+…. aca va la logica
+}
+return <Ítem nombre={elemento.nombre}/>
 
-export default function Listado() {
+/*/
+
+export default function Listado({suma}) {
+
   return (
     <div className='container'>
-      {/* renderizamos los Item aquí */}
+
+  {
+      dataJSON.map((prod)=>{
+        return <Item{...prod} suma={suma} />
+      })}
+    
     </div>
   )
 }
